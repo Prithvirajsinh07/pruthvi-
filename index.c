9080d4626bcb4d20cc9fedb22b57a53091b3bdd5
Prithvirajsinh07 <pgdodiya7208@gmail.com>
2024-05-13 09:54:38 +0530

    changes

--- index.c
+++ index.c
@@ -1,10 +1,10 @@
-#include<stdio.h>
-
-int main()
-{
-    int a=2;
-    float b=2;
-  int c=(int)a+(int)b;
-    printf("%d",c);
-return 0;
-}
+// #include<stdio.h>
+ 
+// int main()
+// {
+//     int a=2;
+//     float b=2;
+//   int c=(int)a+(int)b;
+//     printf("%d",c);
+// return 0;
+// }
